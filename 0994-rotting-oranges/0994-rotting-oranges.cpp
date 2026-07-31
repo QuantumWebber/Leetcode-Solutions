119ba19c3c25d51ca02a75d015b8bf9a04554a8c
JATIN MAGGO <nikilacess4133@gmail.com>
2026-07-31 06:56:58 +0530

    Time: 0 ms (100%), Space: 16.7 MB (70.33%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,67 +1,62 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int rows=grid.size();
-        int cols=grid[0].size();
 
+        queue<pair<int,int>> q;
 
+        int freshCount = 0;   // <-- mistake 1
 
-        queue<pair<int,int>>q; // row and col of rotten orange store kroge 
+        int n = grid.size();
+        int m = grid[0].size();
 
-        int freshCount=0;
-        
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==2){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                if(grid[i][j]==2)
                     q.push({i,j});
-                }else if(grid[i][j]==1){
+
+                else if(grid[i][j]==1)
                     freshCount++;
-                
             }
         }
-    }
 
+        int minutes = 0;
 
-    int minutes=0;
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
 
-    int dr[]={-1,1,0,0};
-    int dc[]={0,0,-1,1};
+        while(!q.empty()){
 
-    while(!q.empty()){
+            int size=q.size();
+            bool rotten=false;
 
-    int size=q.size();
-    bool rottenFound=false;
+            for(int i=0;i<size;i++){
 
-    for(int i=0;i<size;i++){
+                auto [r,c]=q.front();
+                q.pop();
 
-        auto [r,c]=q.front();
-        q.pop();
+                for(int d=0;d<4;d++){
 
-        for(int d=0;d<4;d++){
+                    int nr=r+dr[d];
+                    int nc=c+dc[d];
 
-            int nr=r+dr[d];
-            int nc=c+dc[d];
+                    if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
 
-            if(nr>=0 && nr<rows &&
-               nc>=0 && nc<cols &&
-               grid[nr][nc]==1){
-
-                grid[nr][nc]=2;
-                freshCount--;
-
-                q.push({nr,nc});
-                rottenFound=true;
+                        grid[nr][nc]=2;
+                        freshCount--;
+                        q.push({nr,nc});
+                        rotten=true;
+                    }
+                }
             }
+
+            if(rotten)
+                minutes++;
         }
-    }
 
-    if(rottenFound)
-        minutes++;
-}
+        if(freshCount>0)   
+            return -1;
 
-if(freshCount>0)
-    return -1;
-
-return minutes;
+        return minutes;
     }
 };
