6fd48885c5c5679f8d05be353729c20ec67a3053
JATIN MAGGO <nikilacess4133@gmail.com>
2026-07-31 10:24:41 +0530

    Time: 0 ms (100%), Space: 7.9 MB (65.95%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+
+        string s=to_string(num);
+
+        int ans=num;
+        int n=s.size();
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(s[i],s[j]);
+
+
+                ans=max(ans,stoi(s));
+                swap(s[i],s[j]);
+            }
+        }
+        return ans;
+        
+    }
+};
